5a77626613f829fd71d2312058733f3863d080bd
Matthias Hermann <matthias.h@online.de>
2017-04-18 07:03:07 +0200

    Add executable test template

---
+++ src/test/java/creator/JSONReaderTest.java
@@ -0,0 +1,55 @@
+package creator;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import de.ust.skill.common.java.internal.SkillObject;
+
+public class JSONReaderTest {
+
+	@Test
+	public void test() {
+		Path path = Paths.get(System.getProperty("user.dir"), "src", "test", "resources");
+		path = path.resolve("values.json");
+		
+		try {
+			JSONArray currentJSON = JSONReader.readJSON(path.toFile());
+			for (int i = 0; i < currentJSON.length(); i++) {
+				JSONObject currentTest = currentJSON.getJSONObject(i);
+				SkillObject obj = JSONReader.createSkillObjectFromJSON(currentTest);
+				System.out.println(obj.prettyString());
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
