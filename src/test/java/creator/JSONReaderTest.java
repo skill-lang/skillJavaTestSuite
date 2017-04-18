36208f447adf7bd1600f5429a14218b9dce9785e
Matthias Hermann <matthias.h@online.de>
2017-04-18 08:23:37 +0200

    Modularize JSON tests to granularly handle exceptions

--- src/test/java/creator/JSONReaderTest.java
+++ src/test/java/creator/JSONReaderTest.java
@@ -1,6 +1,6 @@
 package creator;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
@@ -11,59 +11,45 @@ import java.nio.file.Paths;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import de.ust.skill.common.java.internal.SkillObject;
 
 public class JSONReaderTest {
 
+	@Rule //http://stackoverflow.com/a/2935935
+	public final ExpectedException exception = ExpectedException.none();
+
+	private static Path path;
+	private JSONObject currentJSON;
+
 	/**
 	 * Tests the object generation capabilities.
 	 */
-	@Test
-	public void test() {
-		Path path = Paths.get(System.getProperty("user.dir"), "src", "test", "resources");
-		path = path.resolve("values.json");
-		
-		try {
-			JSONArray currentJSON = JSONReader.readJSON(path.toFile());
-			for (int i = 0; i < currentJSON.length(); i++) {
-				JSONObject currentTest = currentJSON.getJSONObject(i);
-				SkillObject obj = JSONReader.createSkillObjectFromJSON(currentTest);
-				System.out.println(obj.prettyString());
-				assertTrue(true);
-			}
-		} catch (JSONException e) {
-			e.printStackTrace();
-			assertTrue(false);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-			assertTrue(false);
-		} catch (IOException e) {
-			e.printStackTrace();
-			assertTrue(false);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			assertTrue(false);
-		} catch (NoSuchMethodException e) {
-			e.printStackTrace();
-			assertTrue(false);
-		} catch (SecurityException e) {
-			e.printStackTrace();
-			assertTrue(false);
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-			assertTrue(false);
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-			assertTrue(false);
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-			assertTrue(false);
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-			assertTrue(false);
-		}
+	@BeforeClass
+	public static void init() throws JSONException, MalformedURLException, IOException {
+		path = Paths.get(System.getProperty("user.dir"), "src", "test", "resources", "test4.json");
 	}
 
+	@Before
+	public void loadNextJSONObject() throws JSONException, MalformedURLException, IOException{
+		this.currentJSON = JSONReader.readJSON(path.toFile());
+	}
+
+	@Test
+	public void jsonTest() throws ClassNotFoundException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		JSONObject currentTest = currentJSON;
+		if(JSONReader.shouldExpectException(currentTest)){
+			System.out.println("There should be an exception coming up!");
+			exception.expect(Exception.class);
+		}
+		SkillObject obj = JSONReader.createSkillObjectFromJSON(currentTest);
+		System.out.println(obj.prettyString());
+		assertTrue(true);
+	}
 }
